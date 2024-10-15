559a3aa88d41f83e954cfe2689984330ca26eff0
Kirill <kirillzh470@gmail.com>
2024-10-15 13:44:23 +0300

    Base skeleton

--- extra_functions.py
+++ extra_functions.py
@@ -1,4 +1,5 @@
 import re
+import pandas as pd
 
 
 def find_group(string: str, mode: str = 'simple') -> bool | dict:
@@ -108,6 +109,130 @@ def flow_distribution(groups: list, hpg: list) -> dict:
                     groups_bool[i] = True
 
     return out
+
+
+def schedule_table_load(path: str) -> tuple:
+    """
+    Загрузка расписания из excel таблицы
+    :param path: путь к файлу
+    :return: turple(disciplines, errors, [header, kaf, year])
+    """
+    table = pd.read_excel(path)
+    table = table.map(lambda x: x.replace('\n', '') if isinstance(x, str) else x)
+    table = table.replace({float('nan'): None})
+
+    group_bools = [False for _ in range(table.shape[0])]
+    disciplines = []
+    errors = []
+    none_titles_rows = []
+
+    header = 'Н/н'
+    kaf = 'Н/н'
+    year = 'Н/н'
+
+    n = 0
+
+    sem = None
+    prev = None
+    labs_base = None
+    discipline = {'title': None, 'flows': [], 'hours_per_flow': [], 'groups': [], 'hours_per_group': [], 'sem': ''}
+    for i, row in enumerate(table.iloc):
+        row = row.to_list()
+        if i == 0:
+            try:
+                header = next(item for item in row if item is not None).lower()
+                ind = header.find('кафедр')
+                ind2 = header.find(' ', ind + 8)
+                kaf = re.sub(r'\D', '', header[ind + 8: ind2])
+                year = header[ind2 + 1: ind2 + 8]
+            except StopIteration:
+                pass
+
+        else:
+            if row[0]:
+                if 'семестр' in row[0].lower():
+                    if 'осен' in row[0].lower():
+                        sem = 'autumn'
+                    elif 'весен' in row[0].lower():
+                        sem = 'summer'
+
+                if find_group(row[0]):
+                    group_bools[i] = True
+                    if row[14] >= 20 and row[23] and row[23] != 2:
+                        errors.append(['W', f'Строка {i + 1}: кол-во часов по ЛР не умножено на два'])
+                    if row[25] and row[25] != row[14] * 2:
+                        errors.append(f'Строка {i + 1}: неверное кол-во часов по КР')
+
+                else:
+                    labs_base = None
+                if 'руководство кафедрой' in row[0].lower():
+                    disciplines.append({'title': row[0], 'flows': [None],
+                                        'hours_per_flow': [row[48]], 'groups': [None],
+                                        'hours_per_group': [row[48]], 'sem': sem})
+
+            if group_bools[i] and not group_bools[i - 1]:
+                discipline = {'title': prev[0], 'flows': [], 'hours_per_flow': [], 'groups': [], 'hours_per_group': [],
+                              'sem': sem}
+                if not prev[0]:
+                    none_titles_rows.append([i - 1, len(disciplines)])
+
+                if row[24] and row[23]:
+                    labs_base = row[24] / row[23]
+            elif group_bools[i] and group_bools[i - 1]:
+                if not check_flow(prev[0], row[0]) and not row[18] and row[11]:
+                    errors.append(['W', f'Строка {i + 1}: неверное распределение групп по потокам'])
+                elif not check_flow(prev[0], row[0]) and row[18]:
+                    if row[24] and row[23]:
+                        labs_base = row[24] / row[23]
+                elif check_flow(prev[0], row[0]):
+                    if (bool(prev[24]) and not bool(row[24]) and
+                            (f'Строка {i + 1}: пропущенные ячейки ЛР у групп одного потока' not in errors)):
+                        errors.append(['E', f'Строка {i + 2}: пропущенные ячейки ЛР у групп одного потока'])
+                    if labs_base and prev[24] and prev[23] and labs_base != prev[24] / prev[23]:
+                        errors.append(['W', f'Строка {i + 1}: неверное кол-во часов по ЛР у групп одного потока'])
+                    if row[11] and prev[11] and row[18] and prev[18] and row[18] != prev[18]:
+                        errors.append(['W', f'Строка {i + 1}: неверное распределение групп по потокам'])
+
+            if prev[0] and find_group(prev[0]):
+                if prev[0] not in discipline['groups']:
+                    discipline['groups'].append(prev[0])
+                    other = 0
+                    if prev[32]:
+                        other += prev[32]
+                    if prev[36]:
+                        other += prev[36]
+                    if prev[37]:
+                        other += prev[37]
+                    if prev[42]:
+                        other += prev[42]
+                    if prev[38]:
+                        other += prev[38]
+
+                    discipline['hours_per_group'].append({'LK': prev[16],
+                                                          'PZ': prev[21],
+                                                          'LR': prev[24],
+                                                          'KR': prev[25],
+                                                          'Other': other
+                                                          })
+
+            if group_bools[i - 1] and not group_bools[i]:
+                fd = flow_distribution(discipline['groups'], discipline['hours_per_group'])
+                discipline.update(fd)
+                disciplines.append(discipline)
+
+        prev = row
+
+    if none_titles_rows:
+        col = table[table.columns[0]].to_list()
+        col.reverse()
+        col_l = len(col)
+        for ntr in none_titles_rows:
+            for cell in col[col_l - ntr[0]:col_l]:
+                if cell and not find_group(cell):
+                    disciplines[ntr[1]]['title'] = cell
+                    break
+
+    return disciplines, errors, [header, kaf, year]
 
 
 if __name__ == '__main__':
--- main.py
+++ main.py
@@ -1,136 +1,339 @@
-import re
-import pandas as pd
-from extra_functions import find_group, check_flow, flow_distribution
+import os
+from pygame import Surface, SurfaceType
+from extra_functions import schedule_table_load
+import pygame
 
-table = pd.read_excel(r'resources/Нагрузка кафедра.xlsx')
-table = table.map(lambda x: x.replace('\n', '') if isinstance(x, str) else x)
-table = table.replace({float('nan'): None})
+parent_dir = os.path.dirname(os.path.abspath(__file__))
+data_dir = os.path.join(parent_dir, 'resources')
 
-group_bools = [False for _ in range(table.shape[0])]
-disciplines = []
-errors = []
-none_titles_rows = []
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 180, 0)
+YELLOW = (255, 195, 63)
+GRAY = (128, 128, 128)
+LIGHT_GRAY = (220, 220, 220)
+BLUE = (0, 0, 255)
 
-header = 'Н/н'
-kaf = 'Н/н'
-year = 'Н/н'
+screen_size = (1280, 720)
+new_size = (1136, 640)
 
-n = 0
+pygame.init()
+clock = pygame.time.Clock()
+screen = pygame.display.set_mode(new_size)
+pygame.display.set_caption("Расписание")
+pygame.mouse.set_visible(True)
 
-sem = None
-prev = None
-labs_base = None
-discipline = {'title': None, 'flows': [], 'hours_per_flow': [], 'groups': [], 'hours_per_group': [], 'sem': ''}
-for i, row in enumerate(table.iloc):
-    row = row.to_list()
-    if i == 0:
-        try:
-            header = next(item for item in row if item is not None).lower()
-            ind = header.find('кафедр')
-            ind2 = header.find(' ', ind + 8)
-            kaf = re.sub(r'\D', '', header[ind + 8: ind2])
-            year = header[ind2 + 1: ind2 + 8]
-        except StopIteration:
-            pass
+pre_screen = pygame.Surface(screen_size)
+
+fps = 60
+block = False
+
+imgs = {}
+fonts = {}
+sheets = {}
+
+
+def preload_resources():
+    global imgs, fonts, sheets
+    full_path = data_dir
+    files = os.listdir(full_path)
+    for file in files:
+        if file.endswith('.png'):
+            im = pygame.image.load(os.path.join(full_path, file)).convert_alpha()
+            im.set_colorkey(BLACK)
+            imgs.update({file[0:-4]: im})
+        elif file.endswith('.jpg'):
+            im = pygame.image.load(os.path.join(full_path, file))
+            imgs.update({file[0:-4]: im})
+        elif file.endswith('.ttf'):
+            fonts.update({file[0:-4]: os.path.join(full_path, file)})
+        elif file.endswith('.xlsx'):
+            sheets.update({file[0:-5]: os.path.join(full_path, file)})
+    print(imgs, fonts, sheets)
+
+class Button:
+    def __init__(self, mode: str, im_on: Surface | SurfaceType | None = None,
+                 im_off: Surface | SurfaceType | None = None, non_blocking_mode: bool = False):
+        """
+        Класс для создания кнопок
+        :param mode: режим кнопки push | toggle
+        :param im_on: картинка для нажатой кнопки
+        :param im_off: картинка для не нажатой кнопки
+        :param non_blocking_mode: отключение блокировки (приминимо только к push) возвращает true пока кнопка нажата
+        """
+        self.block = False
+        self.mode = mode
+        self.pushed = False
+        self.coords = None
+        self.new_size = None
+        self.native_size = screen_size
+        self.im_on = im_on
+        self.im_off = im_off
+        self.non_block = non_blocking_mode
+
+    def set_ims(self, im_on: Surface | SurfaceType | None = None, im_off: Surface | SurfaceType | None = None):
+        """
+        Метод для добавления/изменения картинок уже существующих кнопок
+        :param im_on: картинка для нажатой кнопки
+        :param im_off: картинка для не нажатой кнопки
+        """
+        self.im_on = im_on
+        self.im_off = im_off
+
+    def get_coords(self, x: float = 0, y: float = 0, default=None) -> tuple:
+        """
+        Метод получения позиции относительно позиции кнопки
+        :param x: относительный х
+        :param y: относительный у
+        :param default: значение по умолчанию для тех случаев, когда у кнопки нет позиции
+        :return:
+        """
+        if self.coords:
+            return self.coords[0]+x, self.coords[1]+y
+        else:
+            return default
+
+    def draw(self, text: str, coords: list, text_coords: tuple = (0, 0), color: list = BLACK, size: int = 20,
+             new_size: tuple | str = 'native', box: bool = False, smooth: bool = False):
+        """
+        Метод для отрисовки кнопки. Только после отрисовки кнопка получает координаты!
+        :param text: текст для отображения на кнопке
+        :param coords: позиция кнопки
+        :param text_coords: относительное положение текста на кнопке
+        :param color: цвет обводки кнопки и текста
+        :param size: размер текста
+        :param new_size: размер экрана для маштабирования
+        :param box: включает обводку кнопки
+        :param smooth: включает "мягкое" маштабирование
+        """
+        self.new_size = new_size
+        self.coords = coords
+
+        if self.im_on and self.pushed:
+            images(self.im_on, [self.coords[0], self.coords[1]], [self.coords[2], self.coords[3]], smooth=smooth)
+        elif self.im_off and not self.pushed:
+            images(self.im_off, [self.coords[0], self.coords[1]], [self.coords[2], self.coords[3]], smooth=smooth)
+
+        if box:
+            pygame.draw.rect(pre_screen, color, (coords[0], coords[1], coords[2], coords[3]), 5)
+        if text:
+            write(text, [coords[0] + text_coords[0], coords[1] + text_coords[1]], color, size=size)
+
+    def check(self, global_block: bool = False) -> bool:
+        """
+        Метод для проверки состояния кнопки (требуется предварительно отрисовать кнопку!)
+        :param global_block: включение глобальной блокировки (при нажатии блокируется нажатие ВСЕХ остальных кнопок)
+        :return: bool состояния кнопки
+        """
+        global block
+        pos = pygame.mouse.get_pos()
+        if self.native_size == 'native':
+            if (self.coords[0] < pos[0] < (self.coords[0] + self.coords[2])) and (
+                    self.coords[1] < pos[1] < (self.coords[1] + self.coords[3])):
+                if pygame.mouse.get_pressed(3)[0] and (not self.block or self.non_block):
+                    if global_block and not block:
+                        self.block = True
+                        block = True
+                        if self.mode == 'push':
+                            self.pushed = True
+                        elif self.mode == 'toggle':
+                            self.pushed = not self.pushed
+                    elif not global_block:
+                        self.block = True
+                        if self.mode == 'push':
+                            self.pushed = True
+                        elif self.mode == 'toggle':
+                            self.pushed = not self.pushed
+                elif not pygame.mouse.get_pressed(3)[0] and self.block:
+                    if global_block:
+                        block = False
+                    self.block = False
+                else:
+                    if self.mode == 'push':
+                        self.pushed = False
+            elif self.mode == 'push':
+                self.pushed = False
+
+        else:
+            y_div = self.native_size[1] / new_size[1]
+            x_div = self.native_size[0] / new_size[0]
+            if (self.coords[0] / x_div < pos[0] < (self.coords[0] / x_div + self.coords[2] / x_div)) and (
+                    self.coords[1] / y_div < pos[1] < (self.coords[1] / y_div + self.coords[3] / y_div)):
+                if pygame.mouse.get_pressed(3)[0] and (not self.block or self.non_block):
+                    if global_block and not block:
+                        block = True
+                        self.block = True
+                        if self.mode == 'push':
+                            self.pushed = True
+                        elif self.mode == 'toggle':
+                            self.pushed = not self.pushed
+                    elif not global_block:
+                        self.block = True
+                        if self.mode == 'push':
+                            self.pushed = True
+                        elif self.mode == 'toggle':
+                            self.pushed = not self.pushed
+                elif not pygame.mouse.get_pressed(3)[0] and self.block:
+                    self.block = False
+                    if global_block:
+                        block = False
+                else:
+                    if self.mode == 'push':
+                        self.pushed = False
+            elif self.mode == 'push':
+                self.pushed = False
+
+        return self.pushed
+
+
+def images(filename: Surface | SurfaceType, coords: list = (0, 0), size: str | list = 'native', rotation: float = 0,
+           anchor: str | None = None, smooth: bool = False):
+    """
+    Функция для отрисовки картинок на экране
+    :param filename: предварительно подгруженная картинка (или любая другая поверхность)
+    :param coords: положение якоря картинки
+    :param size: размер картинки после маштабирования (при list[высота] сохраняет соотношение сторон,
+    при list[длина, высота] - нет)
+    :param rotation: вращение картинки относительно геометрического центра ограничивающего прямоугольника
+    :param anchor: якорь картинки (при None левый верхний угол,
+     при 'mid' - геометрический центр ограничивающего прямоугольника)
+    :param smooth: включает "мягкое" маштабирование
+    """
+    back = filename
+    rect = None
+    if size != 'native':
+        if len(size) == 2:
+            back = pygame.transform.scale(back, size)
+        else:
+            old_size = back.get_size()
+            size_y = size[0]
+            size_x = int((old_size[0] * size_y) / old_size[1])
+            if not smooth:
+                back = pygame.transform.scale(back, [size_x, size_y])
+            else:
+                back = pygame.transform.smoothscale(back, [size_x, size_y])
+    if anchor == 'mid':
+        t_size = back.get_size()
+        div_x = int(t_size[0] / 2)
+        div_y = int(t_size[1] / 2)
+        if len(coords) > 2:
+            rotation = coords[2]
+        if rotation != 0:
+            back = pygame.transform.rotozoom(back, rotation, 1)
+            rect = back.get_rect(center=[coords[0], coords[1]])
 
     else:
-        if row[0]:
+        div_x = 0
+        div_y = 0
 
-            if 'семестр' in row[0].lower():
-                if 'осен' in row[0].lower():
-                    sem = 'autumn'
-                elif 'весен' in row[0].lower():
-                    sem = 'summer'
+    if not rect:
+        pre_screen.blit(back, [coords[0] - div_x, coords[1] - div_y])
+    else:
+        pre_screen.blit(back, rect)
 
-            if find_group(row[0]):
-                group_bools[i] = True
-                if row[14] >= 20 and row[23] and row[23] != 2:
-                    errors.append(f'Строка {i + 1}: кол-во часов по ЛР не умножено на два')
-                if row[25] and row[25] != row[14] * 2:
-                    errors.append(f'Строка {i + 1}: неверное кол-во часов по КР')
 
-            else:
-                labs_base = None
-            if 'руководство кафедрой' in row[0].lower():
-                disciplines.append({'title': row[0], 'flows': [None],
-                                    'hours_per_flow': [row[48]], 'groups': [None],
-                                    'hours_per_group': [row[48]], 'sem': sem})
+def write(text: str, coords: list, main_color: list, style: str = 'standard', size: int = 20,
+          anchor: str | None = None):
+    """
+    Функция для отрисовки текста на экране
+    :param text: текст для отрисовки
+    :param coords: положение якоря текста
+    :param main_color: цвет текста
+    :param style: шрифт
+    :param size: размер
+    :param anchor: якорь картинки (при None левый верхний угол,
+     при 'mid' - геометрический центр ограничивающего прямоугольника)
+    """
+    if style == 'standard':
+        font = pygame.font.SysFont('arial', size)
+    elif style == 'bold':
+        font = pygame.font.SysFont('arialполужирный', size)
+    elif style == 'italic':
+        font = pygame.font.SysFont('arialкурсив', size)
+    elif style == 'bold-italic':
+        font = pygame.font.SysFont('arialполужирныйкурсив', size)
+    else:
+        font = pygame.font.Font(style, size)
+    #     sys.exit(('Неверные параметры шрифта!', style))
 
-        if group_bools[i] and not group_bools[i - 1]:
-            discipline = {'title': prev[0], 'flows': [], 'hours_per_flow': [], 'groups': [], 'hours_per_group': [],
-                          'sem': sem}
-            if not prev[0]:
-                none_titles_rows.append([i-1, len(disciplines)])
+    text = font.render(str(text), True, main_color)
+    if not anchor:
+        pre_screen.blit(text, (coords[0], coords[1]))
+    elif anchor == 'mid':
+        sz = text.get_size()
+        pre_screen.blit(text, (coords[0] - (sz[0] / 2), coords[1] - (sz[1] / 2)))
 
-            if row[24] and row[23]:
-                labs_base = row[24] / row[23]
-        elif group_bools[i] and group_bools[i - 1]:
-            if not check_flow(prev[0], row[0]) and not row[18] and row[11]:
-                errors.append(f'Строка {i + 1}: неверное распределение групп по потокам')
-            elif not check_flow(prev[0], row[0]) and row[18]:
-                if row[24] and row[23]:
-                    labs_base = row[24] / row[23]
-            elif check_flow(prev[0], row[0]):
-                if (bool(prev[24]) and not bool(row[24]) and
-                        (f'Строка {i + 1}: пропущенные ячейки ЛР у групп одного потока' not in errors)):
-                    errors.append(f'Строка {i + 2}: пропущенные ячейки ЛР у групп одного потока')
-                if labs_base and prev[24] and prev[23] and labs_base != prev[24] / prev[23]:
-                    errors.append(f'Строка {i + 1}: неверное кол-во часов по ЛР у групп одного потока')
-                if row[11] and prev[11] and row[18] and prev[18] and row[18] != prev[18]:
-                    errors.append(f'Строка {i + 1}: неверное распределение групп по потокам')
 
-        if prev[0] and find_group(prev[0]):
-            if prev[0] not in discipline['groups']:
-                discipline['groups'].append(prev[0])
-                other = 0
-                if prev[32]:
-                    other += prev[32]
-                if prev[36]:
-                    other += prev[36]
-                if prev[37]:
-                    other += prev[37]
-                if prev[42]:
-                    other += prev[42]
-                if prev[38]:
-                    other += prev[38]
+def render(surface: Surface | SurfaceType, size: list | tuple | None = None, default_color: list | tuple = BLACK):
+    """
+    Функция для рендера поверхности на экране
+    :param surface: поверхность для отрисовки на экране
+    :param size: размер поверхности после маштабирования
+    :param default_color: цвет заливки поверхности после отрисовки
+    """
+    screen.fill(default_color)
+    if size and size != screen_size:
+        new_surface = pygame.transform.smoothscale(surface, size)
+    else:
+        new_surface = surface
+    screen.blit(new_surface, [0, 0])
+    pre_screen.fill(default_color)
 
-                discipline['hours_per_group'].append({'LK': prev[16],
-                                                      'PZ': prev[21],
-                                                      'LR': prev[24],
-                                                      'KR': prev[25],
-                                                      'Other': other
-                                                      })
 
-        if group_bools[i - 1] and not group_bools[i]:
-            fd = flow_distribution(discipline['groups'], discipline['hours_per_group'])
-            discipline.update(fd)
-            disciplines.append(discipline)
+def get_mouse() -> tuple:
+    """
+    Функция для получения положения курсора на маштабированом экране
+    :return: tuple(x, y) положения курсора
+    """
+    y_div = screen_size[1] / new_size[1]
+    x_div = screen_size[0] / new_size[0]
+    native = pygame.mouse.get_pos()
+    return native[0] * x_div, native[1] * y_div
 
-    prev = row
 
-    # print(row)
+def buttons_supply(names: list, modes: list) -> dict:
+    """
+    Функция для быстрого создания произвольных словарей кнопок
+    :param names: массив имён кнопок
+    :param modes: массив режимов кнопок
+    :return: dict{keys=names: items=buttons}
+    """
+    out = {}
+    for i in range(len(names)):
+        out.update({names[i]: Button(modes[i])})
 
-if none_titles_rows:
-    col = table[table.columns[0]].to_list()
-    col.reverse()
-    col_l = len(col)
-    for ntr in none_titles_rows:
-        for cell in col[col_l-ntr[0]:col_l]:
-            if cell and not find_group(cell):
-                disciplines[ntr[1]]['title'] = cell
-                break
+    return out
 
-# print(table.iloc[-2])
-print(disciplines, len(disciplines))
-# print(flow_distribution(disciplines[0]['groups'], disciplines[0]['hours_per_group']))
-# print(errors, len(errors))
 
-# print(len(disciplines), group_bools.count(True), n, len(disciplines)+group_bools.count(True)+n)
+class Main:
+    def __init__(self):
+        pass
 
-# ss = []
-#
-# for dis in disciplines:
-#     ss.append(pd.Series(dis))
-#
-# df = pd.DataFrame(ss)
-# df.to_excel('out.xlsx')
+    def draw(self):
+        pass
+
+
+preload_resources()
+
+interface = Main()
+
+done = False
+
+while not done:
+
+    pg_events = pygame.event.get()
+
+    for event in pg_events:  # User did something
+        if event.type == pygame.QUIT:  # If user clicked close
+            done = True
+
+    interface.draw()
+
+    render(pre_screen, new_size, LIGHT_GRAY)
+
+    pygame.display.flip()
+    pygame.display.update()
+    clock.tick(fps)
+
+pygame.display.quit()
